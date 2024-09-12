7d0857b854713af900ce1a618faf1235ecaa83bd
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-09-11 23:05:24 -0400

    commit

--- about.php
+++ about.php
@@ -3,28 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- This is the title for the About me page. -->
     <title>About Me</title>
+    <!-- Linking the CSS file which is placed in the public folder. -->
     <link rel="stylesheet" href="public/style.css">
 </head>
 
+<!-- Content for the body starts from here. -->
 <body>
+
+    <!-- This is the header and the navbar. It is consistent across all pages. -->
     <header class="header">
         <h1 id="nav-title"><span class="h-span">Fit</span>ness Class</h1>
         <nav class="navbar">
             <ul>
                 <li><a href="index.php">Home</a></li>
+
+                <!-- Creating an extra About Me section to display my details. -->
                 <li><a href="about.php">About Me</a></li>
             </ul>
         </nav>
     </header>
 
-<main>
+    <!-- The content for the main body starts from here. -->
+    <main>
+        <?php 
 
-    <?php 
+        ?>
+    </main>
 
-    ?>
-</main>
-
+    <!-- This is the footer and is consistent across all pages. -->
     <footer class="footer">
         <div class="footer-content">
             <p>&copy; 2024 Fitness Class by Sarthak Gupta. All rights reserved.</p>
--- success.php
+++ success.php
@@ -3,25 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration Successful</title>
+    <!-- This is the title for the Success page. -->
+    <title>Registration Successful</title>    
+    <!-- Linking the CSS file which is placed in the public folder. -->
     <link rel="stylesheet" href="public/style.css">
 </head>
 
 <body>
+
+    <!-- This is the header and the navbar. It is consistent across all pages. -->
     <header class="header">
         <h1 id="nav-title"><span class="h-span">Fit</span>ness Class</h1>
         <nav class="navbar">
             <ul>
                 <li><a href="index.php">Home</a></li>
+
+                <!-- Creating an extra About Me section to display my details. -->
                 <li><a href="about.php">About Me</a></li>
             </ul>
         </nav>
     </header>
 
+    <!-- The content for the main body starts from here. -->
     <main>
 
     </main>
 
+    <!-- This is the footer and is consistent across all pages. -->
     <footer class="footer">
         <div class="footer-content">
             <p>&copy; 2024 Fitness Class by Sarthak Gupta. All rights reserved.</p>
