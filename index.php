7c1375af1fca446780086f852b7b6e5bdacb157c
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-09-11 22:45:15 -0400

    commit

--- index.php
+++ index.php
@@ -6,35 +6,35 @@
     <title>Fitness Class</title>
     <link rel="stylesheet" href="public/style.css">
 </head>
+
 <body>
-    
-<header class="header">
-    <h1>Fitness<span class="h-span">Class</span></h1>
-    <nav class="navbar">
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="about.php">About Me</a></li>    
-        </ul>
-    </nav>
-</header>
+    <header class="header">
+        <h1 id="nav-title"><span class="h-span">Fit</span>ness Class</h1>
+        <nav class="navbar">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About Me</a></li>
+            </ul>
+        </nav>
+    </header>
 
-<main>
+    <main>
 
-    <?php 
+        <?php
         function strcat($left, $right)
         {
-        $combinedString = $left . $right;
-        return $combinedString;
+            $combinedString = $left . $right;
+            return $combinedString;
         }
         $first = "This is a ";
         $second = " complete sentence!";
         echo strcat($first, $second);
-        
-    ?>
-</main>
 
-<footer>
+        ?>
+    </main>
 
-</footer>
+    <footer>
+
+    </footer>
 </body>
 </html>
--- success.php
+++ success.php
@@ -3,21 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fitness Class</title>
+    <title>Registration Successful</title>
     <link rel="stylesheet" href="public/style.css">
 </head>
 <body>
-
-
-    <header>
-    <div class="hey">
-        hey
-    </div>
+    <header class="header">
+        <h1 id="nav-title"><span class="h-span">Fit</span>ness Class</h1>
+        <nav class="navbar">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About Me</a></li>
+            </ul>
+        </nav>
     </header>
- 
-    <nav>
-
-    </nav>
 
     <main>
 
