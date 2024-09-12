0eb27eda0b7cade2ef19463108066a88070b38a5
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-09-12 00:05:29 -0400

    updating

--- index.php
+++ index.php
@@ -27,61 +27,65 @@
 
     <!-- The content for the main body starts from here. -->
     <main>
-        <form action="your_php_script.php" method="post">
-            <label for="first_name">First Name:</label>
-            <input type="text" id="first_name" name="first_name" required>
-            <br>
+        <form action="your_php_script.php" method="post" class="form-grid">
+            <div class="form-group">
+                <label for="first_name">First Name:</label>
+                <input type="text" id="first_name" name="first_name" required>
+            </div>
 
-            <label for="last_name">Last Name:</label>
-            <input type="text" id="last_name" name="last_name" required>
-            <br>
+            <div class="form-group">
+                <label for="last_name">Last Name:</label>
+                <input type="text" id="last_name" name="last_name" required>
+            </div>
 
-            <label for="date_of_birth">Date of Birth:</label>
-            <input type="date" id="date_of_birth" name="date_of_birth" required>
-            <br><br>
+            <div class="form-group">
+                <label for="date_of_birth">Date of Birth:</label>
+                <input type="date" id="date_of_birth" name="date_of_birth" required>
+            </div>
 
-            <label for="address">Address:</label>
-            <input type="text" id="address" name="address" required>
-            <br><br>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+            </div>
 
-            <fieldset>
-                <legend>Gender:</legend>
-                <input type="radio" id="male" name="gender" value="Male" required>
-                <label for="male">Male</label>
-                <input type="radio" id="female" name="gender" value="Female">
-                <label for="female">Female</label>
-                <input type="radio" id="other" name="gender" value="Other">
-                <label for="other">Other</label>
-            </fieldset>
-            <br>
+            <div class="form-group">
+                <label for="phone_number">Phone Number:</label>
+                <input type="tel" id="phone_number" name="phone_number" required>
+            </div>
 
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-            <br><br>
+            <div class="form-group">
+                <label for="address">Address:</label>
+                <input type="text" id="address" name="address" required>
+            </div>
 
-            <label for="phone_number">Phone Number:</label>
-            <input type="tel" id="phone_number" name="phone_number" required>
-            <br><br>
+            <div class="form-group">
+                <label for="emergency_contact">Emergency Contact Number:</label>
+                <input type="tel" id="emergency_contact" name="emergency_contact" required>
+            </div>
 
-            <fieldset>
-                <legend>Choose a Batch:</legend>
-                <input type="radio" id="slot1" name="batch" value="Slot1" required>
-                <label for="slot1">Slot 1 (9:00am to 12:00pm)</label>
-                <input type="radio" id="slot2" name="batch" value="Slot2">
-                <label for="slot2">Slot 2 (12:00pm to 3:00pm)</label>
-                <input type="radio" id="slot3" name="batch" value="Slot3">
-                <label for="slot3">Slot 3 (3:00pm to 6:00pm)</label>
-                <input type="radio" id="slot4" name="batch" value="Slot4">
-                <label for="slot4">Slot 4 (6:00pm to 9:00pm)</label>
-            </fieldset>
-            <br>
+            <div class="form-group full-width">
+                <fieldset>
+                    <legend>Gender:</legend>
+                    <label><input type="radio" id="male" name="gender" value="Male" required> Male</label>
+                    <label><input type="radio" id="female" name="gender" value="Female"> Female</label>
+                    <label><input type="radio" id="other" name="gender" value="Other"> Other</label>
+                </fieldset>
+            </div>
 
-            <label for="emergency_contact">Emergency Contact Number:</label>
-            <input type="tel" id="emergency_contact" name="emergency_contact" required>
-            <br><br>
+            <div class="form-group full-width">
+                <fieldset>
+                    <legend>Choose a Batch:</legend>
+                    <label><input type="radio" id="slot1" name="batch" value="Slot1" required> Slot 1 (9:00am to 12:00pm)</label>
+                    <label><input type="radio" id="slot2" name="batch" value="Slot2"> Slot 2 (12:00pm to 3:00pm)</label>
+                    <label><input type="radio" id="slot3" name="batch" value="Slot3"> Slot 3 (3:00pm to 6:00pm)</label>
+                    <label><input type="radio" id="slot4" name="batch" value="Slot4"> Slot 4 (6:00pm to 9:00pm)</label>
+                </fieldset>
+            </div>
 
-            <label for="comment">Additional Comments:</label><br>
-            <textarea id="comment" name="comment" rows="5" cols="40"></textarea>
+            <div class="form-group full-width">
+                <label for="comment">Additional Comments:</label>
+                <textarea id="comment" name="comment"></textarea>
+            </div>
         </form>
     </main>
 
