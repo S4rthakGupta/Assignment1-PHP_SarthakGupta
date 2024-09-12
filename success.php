210cf4f6155b47cb5d6492d5f554c4471406895f
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-09-11 21:49:08 -0400

    updating title

--- success.php
+++ success.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fitness Class</title>
+    <title>Fitness Class Confirmation</title>
 </head>
 <body>
 
