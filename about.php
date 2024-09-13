191598b7e71cb2abe0557dde43f4c11538fbebfc
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-09-12 20:13:55 -0400

    updating about.php

--- about.php
+++ about.php
@@ -30,19 +30,22 @@
     <div class="container">
             <h1 class="main-h1">About Me</h1>
                 <p class="intro-text">
+                    <!-- This is a description for myself. -->
                     I am <i id="about-i">Sarthak Gupta</i>, an aspiring<strong> Full-Stack Developer</strong> with a deep passion for technology and innovation. I specialize in creating dynamic, responsive, and user-friendly web applications using modern web technologies such as HTML, CSS, JavaScript, and frameworks like React and Node.js.
                     <br><br>
                     I am constantly learning and exploring new technologies to stay updated in this rapidly evolving field. I believe in the power of community and love connecting with like-minded professionals to share knowledge, collaborate on projects, and grow together. Currently, I am pursuing a <em>Graduate Certificate in Web Development</em> at Conestoga College in Canada, which has further enhanced my skills.
-
                     <br><br>
                     I'm always open to collaborating on innovative projects or discussing new ideas. Feel free to reach out to me through my portfolio!
                 </p>        
             </div>
 
+        <!-- This is the div in which there is my image. -->
         <div class="hero">
             <div class="hero-img">
                 <img src="public/sarthakgupta.png" alt="This is my Image.">
             </div>        
+
+            <!-- This is the div in which there are two buttons which redirect to my portfolio and YouTube channel. -->
             <div class="content">
                 <p class="intro-text">"Building innovative solutions, one line of code at a time."</p>
                 <div class="buttons">
